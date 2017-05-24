be43ff8ca6b40b89addd98c16572f74ce076eba6
E. Seton Raynor <esetonraynor@gmail.com>
2017-05-24 19:28:01 -0400

    JAWSDB added

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,27 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "languages_db"
-});
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: "languages_db"
+  });
+}
 
-	connection.connect(function(err) {
+connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
   console.log("connected as id " + connection.threadId);
 });
-  
 
 module.exports = connection;
+
+
 
 
 // var express = require("express");
